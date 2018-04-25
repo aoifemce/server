bc3d68e8b4c0c7325c82aae2b9a0a978b7366fa0
Aoife McElwee <mcewee-a3@ulster.ac.uk>
2018-04-25 12:58:13 +0100

    new requests added

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const con = require ('./db');
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
 
 var db = require('./db');
-
+app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/api/clubs', function(request, response){
-     con.query('SELECT  tbl_users.name , tbl_users.email, tbl_clubs.typeOfClub, tbl_clubs.county, tbl_clubs.town, tbl_clubs.clubName FROM tbl_clubAdmins JOIN tbl_users ON tbl_users.userId = tbl_clubAdmins.userId JOIN tbl_clubs ON tbl_clubs.clubId = tbl_clubAdmins.clubId;',
+app.get('/api/clubs', function(request, response, query){
+        const {email} = request.query
+    console.log(email);
+     con.query('SELECT  tbl_users.name , tbl_users.email, tbl_clubs.typeOfClub, tbl_clubs.county, tbl_clubs.town, tbl_clubs.clubName FROM tbl_clubAdmins JOIN tbl_users ON tbl_users.userId = tbl_clubAdmins.userId JOIN tbl_clubs ON tbl_clubs.clubId = tbl_clubAdmins.clubId WHERE tbl_users.email = ?', [email],
     function(error, results){
         if ( error ){
             response.status(400).send('Error in database operation');
@@ -19,6 +22,7 @@ app.get('/api/clubs', function(request, response){
     });
 });
 
+
 app.get('/api/countClubs', function(request, response){
     con.query('SELECT * FROM tbl_clubs;', function(error, results){
         if ( error ){
@@ -26,42 +30,34 @@ app.get('/api/countClubs', function(request, response){
 
         } else {
             response.send(results);
-
         }
     });
 
 });
 
+app.get('/api/orderNumber', function(request, response){
+    con.query('SELECT * FROM tbl_checkout ORDER BY checkout_id DESC LIMIT 1;', function(error, results){
+        if ( error ){
+            response.status(400).send('Error in database operation');
 
-app.get('/api/test', function(request, response){
-    con.query('SELECT  tbl_users.name , tbl_users.email, tbl_clubs.typeOfClub, tbl_clubs.county, tbl_clubs.town, tbl_clubs.clubName FROM tbl_clubAdmins JOIN tbl_users ON tbl_users.userId = tbl_clubAdmins.userId JOIN tbl_clubs ON tbl_clubs.clubId = tbl_clubAdmins.clubId;',
-         function (err, result) {
-          if ( error ){
-             response.status(400).send('Error in database operation');
-
-         } else {
-             response.send(results);
-
-         }
-      });
+        } else {
+            response.send(results);
+        }
+    });
 
 });
 
+app.get('/api/membershipNumber', function(request, response){
+    con.query('SELECT * FROM tbl_membership ORDER BY membershipId DESC LIMIT 1;', function(error, results){
+        if ( error ){
+            response.status(400).send('Error in database operation');
 
-app.get('/api/profileInfo', function(request, response){
-      con.query('SELECT  tbl_users.name , tbl_users.email, tbl_clubs.typeOfClub, tbl_clubs.county, tbl_clubs.town, tbl_clubs.clubName FROM tbl_clubAdmins JOIN tbl_users ON tbl_users.userId = tbl_clubAdmins.userId JOIN tbl_clubs ON tbl_clubs.clubId = tbl_clubAdmins.clubId;',
-      function (err, result) {
-       if ( error ){
-          response.status(400).send('Error in database operation');
+        } else {
+            response.send(results);
+        }
+    });
 
-      } else {
-          response.send(results);
-
-      }
-   });
 });
-
-
 
 app.listen(4000, () => {
     console.log('listening on port 4000')
